9795b0fe9001890ed69d1dbe4702db3e872d1b26
EternalSaul <kxin@thoughtworks.com>
2020-03-17 09:56:57 +0800

    alter deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-oc delete all --selector app=tw-na-pipeline-monitor-grafana &
-oc delete all --selector app=tw-na-pipeline-monitor-prometheus &
-
+oc delete all --selector app=tw-na-pipeline-monitor-grafana &&
+oc delete all --selector app=tw-na-pipeline-monitor-prometheus &&
 oc apply -f tw-na-pipeline-monitor-prometheus-build.yaml &&
 oc apply -f tw-na-pipeline-monitor-grafana-build.yaml &&
 oc start-build tw-na-pipeline-monitor-grafana-build --from-dir grafana &&
