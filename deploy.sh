bb654e81c7bcefe028b3cd7685cab85989b650a6
EternalSaul <kxin@thoughtworks.com>
2020-03-18 13:16:53 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 oc delete all --selector app=tw-na-pipeline-monitor-grafana &&
 oc delete all --selector app=tw-na-pipeline-monitor-prometheus &&
-oc apply -f tw-na-pipeline-monitor-prometheus-build.yaml &&
 oc apply -f tw-na-pipeline-monitor-grafana-build.yaml &&
 oc start-build tw-na-pipeline-monitor-grafana-build --from-dir grafana &&
 oc log bc/tw-na-pipeline-monitor-grafana-build --follow &&
-oc start-build tw-na-pipeline-monitor-prometheus-build --from-dir prometheus &&
-oc log bc/tw-na-pipeline-monitor-prometheus-build --follow &&
-oc new-app tw-na-pipeline-monitor-prometheus &&
-oc autoscale dc/tw-na-pipeline-monitor-prometheus --max 1 &&
-sh tw-na-pipeline-monitor-prometheus-deploy-patch.sh
+oc new-app prom/prometheus --name=tw-na-pipeline-monitor-prometheus &&
+sh prometheus/tw-na-pipeline-monitor-prometheus-deploy-patch.sh &&
 oc new-app tw-na-pipeline-monitor-grafana &&
 oc expose svc/tw-na-pipeline-monitor-grafana &&
 oc expose svc/tw-na-pipeline-monitor-prometheus &&
-sh tw-na-pipeline-monitor-prometheus-persistent-volume-setting.sh
+oc apply -f prometheus/tw-na-pipeline-monitor-prometheus-resource.yaml&&
+oc set volume --add dc/tw-na-pipeline-monitor-prometheus -t=configmap -m /etc/prometheus/prometheus.yml --sub-path=prometheus.yml --name prometheus-config --configmap-name prometheus-configmap --overwrite &&
+sh prometheus/tw-na-pipeline-monitor-prometheus-persistent-volume-setting.sh
+
