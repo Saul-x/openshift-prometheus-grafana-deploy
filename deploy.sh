0a6fa5d7e95b4b3e308c1c41502da686cb23a1b3
EternalSaul <kxin@thoughtworks.com>
2020-03-19 14:55:46 +0800

    rm customize grafana docker image, use mount configmap to config our grafana

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 oc delete all --selector app=tw-na-pipeline-monitor-grafana &&
 oc delete all --selector app=tw-na-pipeline-monitor-prometheus &&
-oc apply -f tw-na-pipeline-monitor-grafana-build.yaml &&
-oc start-build tw-na-pipeline-monitor-grafana-build --from-dir grafana &&
-oc log bc/tw-na-pipeline-monitor-grafana-build --follow &&
 oc new-app prom/prometheus --name=tw-na-pipeline-monitor-prometheus &&
 sh prometheus/tw-na-pipeline-monitor-prometheus-deploy-patch.sh &&
-oc new-app tw-na-pipeline-monitor-grafana &&
+oc new-app grafana/grafana --name=tw-na-pipeline-monitor-grafana &&
 oc expose svc/tw-na-pipeline-monitor-grafana &&
 oc expose svc/tw-na-pipeline-monitor-prometheus &&
-oc apply -f prometheus/tw-na-pipeline-monitor-prometheus-resource.yaml &&
+oc apply -f prometheus/tw-na-pipeline-monitor-prometheus-resources.yaml &&
+oc apply -f grafana/tw-na-pipeline-monitor-grafana-resources.yaml &&
 sh prometheus/tw-na-pipeline-monitor-prometheus-persistent-volume-setting.sh
 
