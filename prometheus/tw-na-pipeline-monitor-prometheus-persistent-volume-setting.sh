fb40c3f83d82bf93a3c16b4ef4c9409a5aa91099
EternalSaul <kxin@thoughtworks.com>
2020-03-19 15:23:04 +0800

    rm useless code

--- prometheus/tw-na-pipeline-monitor-prometheus-persistent-volume-setting.sh
+++ prometheus/tw-na-pipeline-monitor-prometheus-persistent-volume-setting.sh
@@ -17,7 +17,3 @@ fi
 
 #add config file to tw-na-pipeline-monitor-prometheus, the path is /etc/prometheus/prometheus.yml
 oc set volume --add dc/tw-na-pipeline-monitor-prometheus -t=configmap -m /etc/prometheus/prometheus.yml --sub-path=prometheus.yml --name prometheus-config --configmap-name prometheus-configmap
-
-oc set volume --add dc/tw-na-pipeline-monitor-grafana -t=configmap -m /etc/grafana/provisioning/datasources/datasource.yaml --sub-path=datasource.yaml --name grafana-config --configmap-name grafana-configmap
-oc set volume --add dc/tw-na-pipeline-monitor-grafana -t=configmap -m /etc/grafana/provisioning/dashboards/dashboard.yaml --sub-path=dashboard.yaml --name dashboard-provider --configmap-name grafana-configmap
-oc set volume --add dc/tw-na-pipeline-monitor-grafana -t=configmap -m /etc/grafana/provisioning/dashboards/jenkins-metrics-dashboard.json --sub-path=jenkins-metrics-dashboard.json --name jenkins-metrics-dashboard --configmap-name grafana-configmap
