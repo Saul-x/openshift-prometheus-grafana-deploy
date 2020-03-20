befff399f98d76ddd5c98b42fceba44267b8ce2f
EternalSaul <kxin@thoughtworks.com>
2020-03-20 13:25:19 +0800

    update deploy.sh

--- prometheus/tw-na-pipeline-monitor-prometheus-persistent-volume-setting.sh
+++ prometheus/tw-na-pipeline-monitor-prometheus-persistent-volume-setting.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -e
 TW_NA_MONITOR_PVC=`oc get pvc | awk '{if(NR>1) print $1}' | grep monitor-data-pvc`
 PESISTENT_VOLUME_MOUNT_COMMAND='oc set volume dc/tw-na-pipeline-monitor-prometheus --add --type pvc --claim-name monitor-data-pvc --mount-path /prometheus --name monitor-data'
 
@@ -8,7 +7,7 @@ PESISTENT_VOLUME_MOUNT_COMMAND='oc set volume dc/tw-na-pipeline-monitor-promethe
 oc set volume dc/tw-na-pipeline-monitor-prometheus | grep as | awk -F 'as ' '{print $2}'|xargs -I {} oc set volume dc/tw-na-pipeline-monitor-prometheus --remove --name={}
 
 # add psv for tw-na-pipeline-monitor-prometheus, if 'monitor-data-pvc' doesn't exist, create it with --claim-size flag
-if [ $TW_NA_MONITOR_PVC = 'monitor-data-pvc' ];
+if [[ $TW_NA_MONITOR_PVC == 'monitor-data-pvc' ]];
 then
   eval "$PESISTENT_VOLUME_MOUNT_COMMAND"
 else
